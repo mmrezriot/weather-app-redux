cab99262671bb423507c91812facb6727fbbb56e
mmrez <mohammdreza.riot7676@gmail.com>
2025-09-16 11:31:16 +0330

    Implement weather data fetching and loading state in Weather component

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,22 +1,36 @@
 import React, { useState } from 'react';
 import PersianDate from './PersianDate.jsx';
 import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import getWeatherInfo from '../redux/weather/weatherActions.js';
 
 const Weather = () => {
     const [backMode , setBackMode] = useState('cold')
+    const [query , setQuery] = useState('')
+    const {loading , data , error} = useSelector(state => state)
+    const dispatch = useDispatch();
 
-    const {loading , date , error} = useSelector(state => state)
-    console.log(loading , date , error);
+    
+
+   
+const handleSubmit = (e) => {
+   e.preventDefault();
+   dispatch(getWeatherInfo(query)); // فقط dispatch
+
+
+};
 
     return (
         <div className={`app pt-4 container-fluid back_${backMode}`}>
 
             <div className='row justify-content-center py-3 pt-4'>
                 <div className='col-10 col-md-6 col-lg-4 col-xl-3'>
-                    <form>
+                    <form onSubmit={handleSubmit}>
                         <input type="text" 
                         className='search_input w-100 text_color placeholder_color' 
                         placeholder={'نام شهر یا کشور'}
+                        onChange={(e) => setQuery(e.target.value)}
+                        value={query}
                         />
                     </form>
                 </div>
@@ -30,10 +44,18 @@ const Weather = () => {
                 </div>
             </div>
 
-            <div className='row justify-content-center py-3'>
+            {loading ? (
+                <div className='row justify-content-center py-3 pt-4'>
+                    <div className='col-11 col-md-8 col-lg-4 col-xl-3'>
+                        <h3 className='text-center text_color'>در حال بارگذاری...</h3>
+                    </div>
+                </div>
+            ) : data.main ? (
+                <>
+                <div className='row justify-content-center py-3'>
                 <div className='col-9 col-md-6 col-lg-4 col-xl-3'>
                     <div className='temprature_box pt-3'>
-                        <span>15</span> °C
+                        <span>{Math.round(data.main.temp)}</span> °C
                     </div>
                 </div>
             </div>
@@ -43,6 +65,16 @@ const Weather = () => {
                     <h1 className='text-center fa-3x lathin_text text_color'>cloudy</h1>
                 </div>
             </div>
+</>
+            ) : error ? (
+                <div className='row justify-content-center py-3 pt-4'>
+                    <div className='col-11 col-md-8 col-lg-4 col-xl-3'>
+                        <h3 className='text-center text_color'>خطا در دریافت اطلاعات</h3>
+                    </div>
+                </div>  
+            ) : null}
+
+
 
         </div>
     );
